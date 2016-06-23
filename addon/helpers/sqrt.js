11936f2e772384a0f9defce71ffb02b5d35e694c
Robert Wagner <rwagner@jibe.com>
2016-06-23 13:54:26 -0400

    Add sqrt helper

---
+++ addon/helpers/sqrt.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+const {Helper} = Ember;
+
+export function sqrt(params) {
+  return Math.sqrt(params[0]);
+}
+
+export default Helper.helper(sqrt);
---
+++ tests/unit/helpers/sqrt-test.js
@@ -0,0 +1,15 @@
+import { sqrt } from 'dummy/helpers/sqrt';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | sqrt');
+
+// Replace this with your real tests.
+test('square root works', function(assert) {
+  const result = sqrt([25]);
+  assert.equal(result, 5);
+});
+
+test('negative square root is NaN', function(assert) {
+  const result = sqrt([-10]);
+  assert.ok(isNaN(result));
+});
