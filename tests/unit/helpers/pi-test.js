988eb9b0eb3dfbfa81ee4c113e779ff9325a1702
Albert Guo <albert.guo@locusenergy.com>
2016-07-27 14:28:14 -0700

    Deleted Logger

--- tests/unit/helpers/pi-test.js
+++ tests/unit/helpers/pi-test.js
@@ -6,6 +6,5 @@ module('Unit | Helper | pi');
 // Replace this with your real tests.
 test('it works', function(assert) {
   let result = pi();
-  console.log(result);
   assert.equal(result, 3.141592653589793, 'The Pi helper works as expected');
 });
