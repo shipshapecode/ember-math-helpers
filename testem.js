8a812241b0dbc65463a58dcc52d55454776a41e0
Robert Wagner <rwwagner90@gmail.com>
2016-08-03 08:21:40 -0400

    Add back blanket

--- testem.js
+++ testem.js
@@ -1,12 +1,12 @@
-/*jshint node:true*/
+/* jshint node:true*/
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Firefox"
+  'framework': 'qunit',
+  'test_page': 'tests/index.html?hidepassed&coverage',
+  'disable_watching': true,
+  'launch_in_ci': [
+    'Firefox'
   ],
-  "launch_in_dev": [
-    "Firefox"
+  'launch_in_dev': [
+    'Firefox'
   ]
 };
