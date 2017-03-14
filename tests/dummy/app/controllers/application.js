ce5edad8d9b43f14dce354c701e714df226a7dc2
Robert Wagner <rwwagner90@gmail.com>
2017-03-14 08:44:43 -0400

    Convert all basic examples

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,14 +2,8 @@ import Controller from 'ember-controller';
 
 export default Controller.extend({
   addTemplate: '{{add 1 2}}',
-  add1: 1,
-  add2: 2,
-  div1: 20,
-  div2: 10,
-  mod1: 11,
-  mod2: 10,
-  mult1: 6,
-  mult2: 6,
-  sub1: 10,
-  sub2: 2
+  divTemplate: '{{div 20 10}}',
+  modTemplate: '{{mod 11 10}}',
+  multTemplate: '{{mult 6 6}}',
+  subTemplate: '{{sub 10 2}}'
 });
