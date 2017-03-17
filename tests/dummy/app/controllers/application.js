da2232a9b0eddc40a719d7f53dfe98567fb9206f
Robert Wagner <rwwagner90@gmail.com>
2017-03-16 21:28:46 -0400

    Add composable helpers

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -5,5 +5,8 @@ export default Controller.extend({
   divTemplate: '{{div 20 10}}',
   modTemplate: '{{mod 11 10}}',
   multTemplate: '{{mult 6 6}}',
-  subTemplate: '{{sub 10 2}}'
+  subTemplate: '{{sub 10 2}}',
+
+  composableSub: '{{sub 10 1 2 3}}',
+  composableComplex: '{{mult (div (add 15 5) 2) 10}}'
 });
