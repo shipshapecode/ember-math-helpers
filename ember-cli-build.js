74db395b9055c0ff09679e0ae498c339ea2cb85b
Robert Wagner <rwwagner90@gmail.com>
2017-08-16 12:18:20 -0400

    Options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,14 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  const app = new EmberAddon(defaults, {
-    vendorFiles: { 'jquery.js': null }
-  });
+  let project = defaults.project;
+  let options = {};
+
+  if (project.findAddonByName('ember-native-dom-event-dispatcher') && process.env.DEPLOY_TARGET === undefined) {
+    options.vendorFiles = { 'jquery.js': null };
+  }
+
+  let app = new EmberAddon(defaults, options);
 
   /*
     This build file specifies the options for the dummy test app of this
