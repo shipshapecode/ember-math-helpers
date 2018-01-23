b8ed9033250b8409138bd75c3ccbd79ff0f7a39f
Robert Wagner <rwwagner90@gmail.com>
2018-01-22 22:22:11 -0500

    Ember 3.0 beta

--- tests/unit/helpers/min-test.js
+++ tests/unit/helpers/min-test.js
@@ -2,14 +2,11 @@ import { min } from 'dummy/helpers/min';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | min', function() {
-  // Replace this with your real tests.
-  test('it works', function(assert) {
+  test('min works', function(assert) {
     assert.equal(min([3, 2, 3, 5, 6, 42, 3, 6, 7]), 2, 'accepts multiple arguments');
 
     assert.equal(min([42]), 42, 'accepts one argument');
 
     assert.equal(min([]), Infinity, 'accepts zero arguments');
-
-    assert.equal(min({}), Infinity, 'accepts an object');
   });
 });
