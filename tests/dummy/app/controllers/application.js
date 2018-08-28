e45970ab936cbba0765bd16476210251c35ad786
Brian Sipple <brsipple42@gmail.com>
2018-08-27 18:19:03 -0700

    add `gcd` helper example to dummy app

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -7,6 +7,8 @@ export default Controller.extend({
   multTemplate: '{{mult 6 6}}',
   subTemplate: '{{sub 10 2}}',
 
+  gcdTemplate: '{{gcd 10 2}}',
+
   composableSub: '{{sub 10 1 2 3}}',
   composableComplex: '{{mult (div (add 15 5) 2) 10}}'
 });
