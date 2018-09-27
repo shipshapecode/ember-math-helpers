390d33c84003e6579f363ea46b504c3154e12dcd
Robert Wagner <rwwagner90@gmail.com>
2018-09-26 20:19:13 -0400

    Remove ghpages deploy

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -20,9 +20,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-    'ember-cli-addon-docs': {
-      disableGeneratedDocs: false
     }
   };
 
